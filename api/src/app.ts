a3e7bebab5523919378a94343ca8255e01dbd863
ggg6r34t <ghyorka.2020@gmail.com>
2023-07-11 23:16:24 +0200

    refactor(app): Code refactorings for app
    This commit removes the frontend code in the app file and refactors app import statement in the server file.

--- api/src/app.ts
+++ api/src/app.ts
@@ -8,7 +8,7 @@ import productRouter from "./routes/products";
 import userRouter from "./routes/users";
 import orderRouter from "./routes/orders";
 
-export const app: Express = express();
+const app: Express = express();
 app.use(express.json());
 app.use(cors());
 app.use(passport.initialize());
@@ -21,7 +21,5 @@ app.use("order", orderRouter);
 
 // error handler
 app.use(apiErrorHandler);
-const port = 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+export default app;
