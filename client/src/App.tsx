cd5ccd704b909461ebde62274cac4a48570e7fcb
ggg6r34t <ghyorka.2020@gmail.com>
2023-07-17 09:16:29 +0200

    feat(error): Add NotFound error page
    This commit adds NotFound error page and refactors imports in the app file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,10 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./components/theme/theme";
 import NavBar from "./components/layout/NavBar";
 import Footer from "./components/layout/Footer";
-import Home from "./pages/Home";
-import ProductList from "./pages/Product";
-import ProductDetailsList from "./pages/ProductDetails";
+import PageNotFound from "./components/pages/error/NotFound";
+import HomePage from "./pages/Home";
+import ProductListPage from "./pages/Product";
+import ProductDetailsListPage from "./pages/ProductDetails";
 import LoginPage from "./pages/Login";
 import RegisterPage from "./pages/Register";
 import UserInfoPage from "./pages/UserAccount";
@@ -17,12 +18,13 @@ function App() {
       <ThemeProvider theme={theme}>
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/products/:name" element={<ProductDetailsList />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductListPage />} />
+          <Route path="/products/:name" element={<ProductDetailsListPage />} />
           <Route path="/account/login" element={<LoginPage />} />
           <Route path="/account/register" element={<RegisterPage />} />
           <Route path="/account" element={<UserInfoPage />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
         <Footer />
       </ThemeProvider>
