a0831c498b3d3f9ca4bde683782ace5a8b3134a5
ggg6r34t <ghyorka.2020@gmail.com>
2023-07-25 00:05:01 +0200

    chore(api_ts): Update deployed backend URL

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://backend-xbfk.onrender.com"
+    ? "https://backend-xyp9.onrender.com"
     : "http://localhost:8000";
