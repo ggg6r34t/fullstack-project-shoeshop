8fcd9961d37dbf368f076f4e5bcfb32d3d64bf6b
ggg6r34t <ghyorka.2020@gmail.com>
2023-07-24 21:42:52 +0200

    chore(api_ts): Add deployed backend URL

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,6 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
-
-//     export const BASE_URL =
-//   process.env.NODE_ENV === "production"
-//     ? "deployed backend URL"
-//     : "http://localhost:8000";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://backend-xbfk.onrender.com"
+    : "http://localhost:8000";
