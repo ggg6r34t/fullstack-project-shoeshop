5adf0c4593aa45f9c8dd928409497cce2f1880eb
ggg6r34t <ghyorka.2020@gmail.com>
2023-07-28 13:24:02 +0200

    style(progressBar): Code style changes

--- client/src/components/product/ProductList.tsx
+++ client/src/components/product/ProductList.tsx
@@ -1,8 +1,8 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Box, Container, CircularProgress, Grid } from "@mui/material";
-// import styled from "styled-components";
+import { Box, Container, Grid, LinearProgress } from "@mui/material";
+import styled from "styled-components";
 
 import { AppDispatch, RootState } from "../../redux/store";
 import {
@@ -11,9 +11,16 @@ import {
 } from "../../redux/thunk/products";
 import ProductItem from "./ProductItem";
 
+const StyledLinearProgressBar = styled(LinearProgress)`
+  & .MuiLinearProgress-bar {
+    background-color: #044606 !important;
+  }
+`;
+
 export default function ProductList() {
   const products = useSelector((state: RootState) => state.products.products);
   const isLoading = useSelector((state: RootState) => state.products.isLoading);
+  const [progress, setProgress] = useState(0);
 
   const fetchDispatch = useDispatch<AppDispatch>();
 
@@ -23,22 +30,41 @@ export default function ProductList() {
   useEffect(() => {
     fetchDispatch(fetchProductData());
     fetchDispatch(fetchProductByCategory(category));
+
+    const timer = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === 100) {
+          return 0;
+        }
+        const diff = Math.random() * 10;
+        return Math.min(oldProgress + diff, 100);
+      });
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
   }, [fetchDispatch, category, param]);
 
   if (isLoading) {
     return (
-      <Container sx={{ mt: 15, minHeight: 950 }}>
-        <Box
+      <Box
+        sx={{
+          width: "100%",
+          position: "relative",
+          top: "157.5px",
+        }}
+      >
+        <StyledLinearProgressBar
+          variant="determinate"
+          value={progress}
           sx={{
-            position: "relative",
-            top: "250px",
-            left: "400px",
-            display: "inline-flex",
+            "&.MuiLinearProgress-root": {
+              backgroundColor: "#04460638",
+            },
           }}
-        >
-          <CircularProgress size={300} />
-        </Box>
-      </Container>
+        />
+      </Box>
     );
   }
 
